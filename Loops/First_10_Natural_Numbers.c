5e836dc29ea367934e108e9dc018a93d4e718511
PrerakPithadiya <139850989+PrerakPithadiya@users.noreply.github.com>
2023-11-29 21:04:26 +0530

    Add files via upload

--- Loops/First_10_Natural_Numbers.c
+++ Loops/First_10_Natural_Numbers.c
@@ -1,1 +1,12 @@
-
+#include<stdio.h>
+int main(){
+    printf("The First 10 Natural Numbers are :\n[");
+    for(int i = 1; i <= 10; i++){
+        printf("%d", i);
+        if(i < 10){
+            printf(", ");
+        }
+    }
+    printf("]\n");
+    return 0;
+}
