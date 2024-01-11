d1995bf8baf0f2f3f436d38b7396823585ba64d6
PrerakPithadiya <139850989+PrerakPithadiya@users.noreply.github.com>
2024-01-11 22:21:32 +0530

    Add files via upload

--- Functions/isPrime.cpp
+++ Functions/isPrime.cpp
@@ -1,1 +1,30 @@
+#include<stdio.h>
 
+int isPrime (int n);
+
+int main () {
+	int n;
+	printf("Enter the Number : ");
+	scanf("%d", &n);
+	if (isPrime(n) == 1) {
+		printf("The given Number '%d' is Prime!", n);
+	} else {
+		printf("The given Number '%d' isn't Prime!'", n);
+	}
+	return 0;
+}
+
+int isPrime (int n) {
+	if (n == 1) {
+		return 0;
+	} if (n == 2 || n == 3) {
+		return 1;
+	} if (n % 2 == 0 || n % 3 == 0) {
+		return 0;
+	} for (int i = 6; i * i <= n; i += 6) {
+		if (n % i == 0 || n % (i + 2) == 0) {
+			return 0;
+		}
+	}
+	return 1;
+}
