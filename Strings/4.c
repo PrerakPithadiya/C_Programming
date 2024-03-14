37bd130616a4553425f7b80339b99e29934caa0b
PrerakPithadiya <139850989+PrerakPithadiya@users.noreply.github.com>
2024-03-15 04:19:01 +0530

    Update 4.c

--- Strings/4.c
+++ Strings/4.c
@@ -2,13 +2,28 @@
 #include <string.h>
 
 int main() {
-    char str1[50] = "Hello, ";
-    char str2[] = "World!";
-
-    // Using strcat() to concatenate strings
-    strcat(str1, str2);
-
-    printf("Concatenated string: %s\n", str1);
-
+    char str1[] = "Hello";
+    char str2[] = "World";
+    char str3[] = "Hello";
+    
+    int result1 = strcmp(str1, str2);
+    int result2 = strcmp(str1, str3);
+    
+    if (result1 == 0) {
+        printf("str1 and str2 are equal\n");
+    } else if (result1 < 0) {
+        printf("str1 is less than str2\n");
+    } else {
+        printf("str1 is greater than str2\n");
+    }
+    
+    if (result2 == 0) {
+        printf("str1 and str3 are equal\n");
+    } else if (result2 < 0) {
+        printf("str1 is less than str3\n");
+    } else {
+        printf("str1 is greater than str3\n");
+    }
+    
     return 0;
 }
